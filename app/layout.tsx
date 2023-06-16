3ecb9fbc126836d656c8089a6e786504a99618a1
fahimehsd <fahimehsadeghi3@gmail.com>
2023-06-16 22:02:45 +0330

    footer done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Car Hub",
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body className="relative">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
